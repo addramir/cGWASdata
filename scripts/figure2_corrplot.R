74b10235ebefa0413153525d3cf11bc46a9b8175
Yakov <drosophila.simulans@gmail.com>
2017-12-07 16:39:41 +0700

    data upload

--- scripts/figure2_corrplot.R
+++ scripts/figure2_corrplot.R
@@ -1,11 +1,13 @@
-#####
-##### Butiful example - figure 2 
-#####
+
 library(corrplot)
 library(ppcor)
 par(mfrow=c(1,2))
 
-xb=read.table("20161110_big.txt",header=T,stringsAsFactors = F)
+xb=read.table("data_table.txt",header=T,stringsAsFactors = F)
+setwd("...")
+load("data_for_plot.RData")
+
+par(mfrow=c(1,2))
 #FADS1
 i=which(xb[,"Gene"]=="FADS1")
 snp=xb[i,"SNP"]
@@ -20,7 +22,14 @@ Mpc=Mpc[colnames(M),colnames(M)]
 M <- cor(sdta[,c(trait,"snp",cvrts)])
 M[lower.tri(M)]=0
 M[2:(length(cvrts)+2),1]=lm(paste(trait,"~snp+",paste(cvrts,collapse="+")),sdta)$coef[2:(length(cvrts)+2)]
-corrplot(M, method="circle",cl.pos="b")
+
+colnames(M)[2]=rownames(M)[2]="SNP"
+colnames(M)[1]=rownames(M)[1]="lysoPC a C20:4"
+colnames(M)[3]=rownames(M)[3]="lysoPC a C20:3"
+diag(M)=0
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(M,cl.pos="b",na.label="X",method = "square",
+         addCoef.col = "black",col = col(200),number.cex = 1, tl.cex=2,tl.col=c("blue","darkblue","red"))
 
 #ETFDH
 i=which(xb[,"Gene"]=="ETFDH")
@@ -33,5 +42,10 @@ sdta=cbind(snp=dta[,snp],phedata[,c(cvrts,trait)])
 M <- cor(sdta[,c(trait,"snp",cvrts)])
 M[lower.tri(M)]=0
 M[2:(length(cvrts)+2),1]=lm(paste(trait,"~snp+",paste(cvrts,collapse="+")),sdta)$coef[2:(length(cvrts)+2)]
-corrplot(M, method="circle",cl.pos="b")
+
+colnames(M)[2]=rownames(M)[2]="SNP"
+diag(M)=0
+col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+corrplot(M,cl.pos="b",na.label="X",method = "square",
+         addCoef.col = "black",col = col(200),number.cex = 1, tl.cex=2,tl.col=c("blue","darkblue","red","red"))
 
